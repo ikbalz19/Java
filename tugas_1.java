e639498663d2652bb3c8ab6351c162f6ca273582
ikbalz19 <miqbalnugraha0112@gmail.com>
2024-11-18 10:03:00 +0700

    Tugas Java

---
+++ tugas_1.java
@@ -0,0 +1,15 @@
+public class tempat 
+public static void main (string<)args>
+string nama "ikbal nugraha",
+system out printin <ikbal nugraha lenght
+}
+ }
+ java string is empty
+ public class tempat {
+     public static void main <string<>args>{
+         string nama 1 = "ikbal" ;
+         string nama 2 = "ikbal" ;
+         system .out printin < nama 1. 11 empty<1>;
+         system. out printin <nama 1. 11 empty <1>
+     }
+ }
